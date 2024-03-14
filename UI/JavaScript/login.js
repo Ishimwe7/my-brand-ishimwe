d4b60b4c80e4adf7a585253d4b168b0bf70b6a9c
Ishimwe Pacifique <ishimweinstein@gmail.com>
2024-03-14 14:49:15 +0200

    all validation codes

--- UI/JavaScript/login.js
+++ UI/JavaScript/login.js
@@ -1,13 +1,28 @@
+
+window.addEventListener("load", (event) => {
+    // if (event.target.readyState === "complete") {
+    initApp();
+    // }
+});
+
+const initApp = () => {
+    //Add listeners
+    const adminLoginForm = document.getElementById("login-form");
+    adminLoginForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        authenticateUser();
+    })
+}
+
 function authenticateUser() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
     if (email === "ishimweinstein@gmail.com" && password === "Admin") {
         sessionStorage.setItem('sessionToken', email);
-        window.location.href = 'adminDashboard.html';
+        window.location = "../pages/adminDashboard.html";
     } else {
-        // alert('Invalid username or password');
-        var invalid = document.getElementById("invalidLogin");
+        const invalid = document.getElementById("invalidLogin");
         invalid.textContent = "Invalid Credentials !!"
         invalid.style.display = "block"
     }
