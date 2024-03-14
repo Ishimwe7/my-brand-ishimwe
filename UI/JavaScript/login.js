bbaca27f016566efbb5d7160ed7e420ffb24d91e
Ishimwe Pacifique <ishimweinstein@gmail.com>
2024-03-14 14:06:36 +0200

    commit

--- UI/JavaScript/login.js
+++ UI/JavaScript/login.js
@@ -2,13 +2,8 @@ function authenticateUser() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
 
-    // Check if the stored password matches the entered password
-    // if (localStorage.getItem(email) === password) {
-    if (localStorage.getItem(email) === password) {
-        // Store a session token (for simplicity, using the username)
-        localStorage.setItem('sessionToken', email);
-
-        // Redirect to the dashboard or perform other actions
+    if (email === "ishimweinstein@gmail.com" && password === "Admin") {
+        sessionStorage.setItem('sessionToken', email);
         window.location.href = 'adminDashboard.html';
     } else {
         // alert('Invalid username or password');
