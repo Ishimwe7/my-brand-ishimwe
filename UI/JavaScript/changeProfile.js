37ca4a7b89eccd64f8b9997796aa9c87f051790d
Ishimwe Pacifique <ishimweinstein@gmail.com>
2024-03-15 12:58:07 +0200

    made slight changes

--- UI/JavaScript/changeProfile.js
+++ UI/JavaScript/changeProfile.js
@@ -3,7 +3,7 @@ import Profile from "./profile.js";
 const form = document.getElementById("change-profile-form");
 form.addEventListener(("submit"), (event) => {
     event.preventDefault();
-    const text = document.getElementById("intro");
+    // const text = document.getElementById("intro");
     const picture = document.getElementById("picture");
 
     let imageUrl = null;
@@ -11,11 +11,11 @@ form.addEventListener(("submit"), (event) => {
     imageReader.addEventListener('load', () => {
         imageUrl = imageReader.result;
         const profile = new Profile();
-        profile.setText(text.value);
+        // profile.setText(text.value);
         profile.setPicture(imageUrl);
         localStorage.setItem("profile", JSON.stringify(profile));
         alert("Profile Updated Successfully !!");
-        text.textContent = '';
+        // text.textContent = '';
         picture.value = '';
         location.reload();
     });
